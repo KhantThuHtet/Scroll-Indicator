91133940a585f6d1473a444cf883ec2ef7b55a24
KhantThuHtet <khantthu7007@gmail.com>
2021-12-30 12:31:19 +0630

    done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 let htmlFullHeight = document.body.clientHeight;
 let viewportHeight = window.innerHeight;
 let leftHeight = htmlFullHeight - viewportHeight;
+let progressBar = document.getElementById('progress-bar');
 
 console.log(leftHeight);
 
 window.addEventListener('scroll', ()=>{
-    
+    let value = (scrollY / leftHeight) * 100;
+    progressBar.style.width = value + '%';
+});
+
+window.addEventListener('resize', ()=>{
+    htmlFullHeight = document.body.clientHeight;
+    viewportHeight = window.innerHeight;
+    leftHeight = htmlFullHeight - viewportHeight;
+    progressBar = document.getElementById("progress-bar");
 });
